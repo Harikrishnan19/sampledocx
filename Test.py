621473a51ba4954b0890ab303770b093064eb355
Harikrishnan19 <harikrishnan97arun@gmail.com>
2020-07-01 19:17:05 +0530

    second version of test

--- Test.py
+++ Test.py
@@ -4,3 +4,7 @@ c=a+b
 print("the addition of 16 and 7 is ", c)
 d=b-a
 print("the Subtraction of 16 and 7 is ", d)
+e=a*b
+print("the multiplication of 16 and 7 is ", e)
+f=a/b
+print("the division of 7 by 16 is ", f)
