0233783ee2ee7f7e0befaf7a9be9abf2c4dc78fa
Harikrishnan19 <harikrishnan97arun@gmail.com>
2020-07-01 21:48:28 +0530

    simplified version of test

--- Test.py
+++ Test.py
@@ -1,10 +1,6 @@
 a=7
 b=16
-c=a+b
-print("the addition of 16 and 7 is ", c)
-d=b-a
-print("the Subtraction of 16 and 7 is ", d)
-e=a*b
-print("the multiplication of 16 and 7 is ", e)
-f=a/b
-print("the division of 7 by 16 is ", f)
+print("the addition of 16 and 7 is ", a+b)
+print("the Subtraction of 16 and 7 is ", b-a)
+print("the multiplication of 16 and 7 is ", a*b)
+print("the division of 7 by 16 is ", a/b)
